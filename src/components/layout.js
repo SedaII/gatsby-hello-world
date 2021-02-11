3fc4a6c147342478757b57997174d6a6d9b8d98f
SedaII <i.messnaoui@gmail.com>
2021-02-11 20:30:47 +0100

    Tuto Pt 3 - Creating Nested Layout Components

---
+++ src/components/layout.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import layoutStyles from "./layout.module.css"
+
+const ListLink = (props) => (
+  <li style={{ display: `inline-block`, marginRight: `1rem`}}>
+  <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
+export default function Layout({ children }) {
+  return (
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <header style={{ marginBottom: `1.5rem` }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline` }}>MySweetSite</h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children}
+      <footer className={layoutStyles}>
+      Made from <Link to="https://www.gatsbyjs.com/docs/tutorial/part-three/">https://www.gatsbyjs.com/docs/tutorial/part-three/</Link>
+      </footer>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header.js"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <Layout>
       <Link to="/contact/">Contact</Link>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="random image"/>
-    </div>
+      <img src="https://source.unsplash.com/random/400x200" alt="random"/>
+    </Layout>
+    
   )
 }
